1d9422e970769fe65258f6a6aaecfbb827b7763b
junshibrain <clayton.wang@digitalbrain.cn>
2022-06-10 10:51:48 +0800

    loading latest code

--- label_studio/frontend/src/pages/DataManager/DataManager.js
+++ label_studio/frontend/src/pages/DataManager/DataManager.js
@@ -58,8 +58,8 @@ const initializeDataManager = async (root, props, params) => {
     root,
     toolbar: "actions columns filters ordering pre-button label-button loading-possum error-box  | refresh import-button export-button view-toggle",
     projectId: params.id,
-    apiGateway: `${window.APP_SETTINGS.hostname}/api/dm`,
-    // apiGateway: `http://124.71.161.146:8080/api/dm`,
+    // apiGateway: `${window.APP_SETTINGS.hostname}/api/dm`,
+    apiGateway: `http://124.71.161.146:8080/api/dm`,
     apiVersion: 2,
     project: params.project,
     polling: !window.APP_SETTINGS,
@@ -240,6 +240,12 @@ export const DataManagerPage = ({ ...props }) => {
         bare={true}
         allowClose={false}
         animateAppearance={false}
+        onHide={ 
+          async () => {
+            await dataManagerRef?.current?.store?.fetchProject({ force: true, interaction: 'refresh' });
+            await dataManagerRef?.current?.store?.currentView?.reload();
+          }
+        }
         style={{
           width: '150px',
           minWidth:'150px',
