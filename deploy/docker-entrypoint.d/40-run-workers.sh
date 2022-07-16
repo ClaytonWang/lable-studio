0f4a71ec0c0239155102424905553f0bf0ddfc65
junshibrain <junshi.wang@digitalbrain.cn>
2022-07-16 16:10:21 +0800

    update

--- deploy/docker-entrypoint.d/40-run-workers.sh
+++ deploy/docker-entrypoint.d/40-run-workers.sh
@@ -4,11 +4,11 @@ set -e ${DEBUG:+-x}
 if [ -n "${POSTGRE_HOST:-}" ]; then
   echo >&3 "=> up workers..."
   #3 worker
-  python3 label_studio/manage.py rqworker pre_tags >&3
+  python3 /label-studio/label_studio/manage.py rqworker pre_tags >&3
 
-  python3 label_studio/manage.py rqworker algorithm_clean >&3
+  python3 /label-studio/label_studio/manage.py rqworker algorithm_clean >&3
 
-  python3 label_studio/manage.py rqworker prompt >&3
+  python3 l/label-studio/abel_studio/manage.py rqworker prompt >&3
   echo >&3 "=> workers up completed."
 else
   echo >&3 "=> Skipping run workers."
