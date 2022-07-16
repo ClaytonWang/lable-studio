8220e2c5e076ea36ac3a1b1e84f465b896175445
junshibrain <junshi.wang@digitalbrain.cn>
2022-07-16 18:35:25 +0800

    update redis

--- label_studio/core/settings/dev_settings.py
+++ label_studio/core/settings/dev_settings.py
@@ -22,25 +22,25 @@ DATABASES = {
 
 RQ_QUEUES = {
     'default': {
-        'HOST': 'redis-dev',
+        'HOST': 'redis',
         'PORT': 6379,
         'DB': 0,
         'DEFAULT_TIMEOUT': 18000,
     },
     'prediction': {
-        'HOST': 'redis-dev',
+        'HOST': 'redis',
         'PORT': 6379,
         'DB': 1,
         'DEFAULT_TIMEOUT': 3600 * 24 * 2,
     },
     'algorithm_clean': {
-        'HOST': 'redis-dev',
+        'HOST': 'redis',
         'PORT': 6379,
         'DB': 1,
         'DEFAULT_TIMEOUT': 3600 * 24 * 2,
     },
     'prompt': {
-        'HOST': 'redis-dev',
+        'HOST': 'redis',
         'PORT': 6379,
         'DB': 1,
         'DEFAULT_TIMEOUT': 18000,
