13c2ff9bb8cb9f0dd73b44c3ebcfad0338ea7ac9
junshi <junshi.wang@digitalbrain.cn>
2022-07-28 02:16:32 +1300

    add org page

---
+++ label_studio/frontend/src/pages/Organization/OrgManagerPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrgManager = () => {
+  return (
+    <div>OrgManager</div>
+  );
+};
+
+export const OrgManagerPage = {
+  title: "组织列表",
+  path: "/org-manage",
+  component: OrgManager,
+};
