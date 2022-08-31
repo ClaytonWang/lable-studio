29c98421c4f251d8fd86e4b8dc662494bdf5ec92
junshi <junshi.wang@digitalbrain.cn>
2022-08-31 20:07:11 +1300

    update

--- label_studio/frontend/src/pages/DataManager/DataOperate/HumanInTheLoop/List.js
+++ label_studio/frontend/src/pages/DataManager/DataOperate/HumanInTheLoop/List.js
@@ -1,5 +1,6 @@
 import { useCallback ,useContext, useEffect,useRef,useState } from "react";
-import { Button, Popconfirm, Select, Space,Tooltip } from "antd";
+import { Popconfirm, Select, Space, Tooltip } from "antd";
+import { Button } from "@/components/Button/Button";
 import { ExclamationCircleOutlined,QuestionCircleOutlined } from "@ant-design/icons";
 import { ProTable } from "@ant-design/pro-components";
 import { PlusOutlined } from "@ant-design/icons";
@@ -10,7 +11,7 @@ import { Userpic } from '@/components';
 import { format } from 'date-fns';
 const { Option } = Select;
 
-export default ({ onCancel, onEvaluate, onTrain }) => {
+export default ({ onCancel, onEvaluate, onTrain,onAccuracy }) => {
   const api = useContext(ApiContext);
   const { project } = useProject();
   const [prevModels, setPrevModels] = useState([]);
@@ -131,24 +132,24 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
         headerTitle={(
           <Space>
             <Select key="0" placeholder="训练前模型" onChange={modelChange} >
-              <Option key="">不限</Option>
+              <Option value="">不限</Option>
               {prevModels.map((model) => (
-                <Option key={model.id}>{model.title} { model.version}</Option>
+                <Option key={model.id} title={ model.title + model.version}>{model.title}{ model.version}</Option>
               ))}
             </Select>
             <Select key="1" placeholder="操作人" onChange={operatorChange} >
-              <Option key="">不限</Option>
+              <Option value="">不限</Option>
               {operators.map((user) => (
                 <Option key={user.user_id}>{user.username}</Option>
               ))}
             </Select>
             <Select key="2" placeholder="是否训练" onChange={isTrainChange} >
-              <Option key="">不限</Option>
-              <Option key="true">是</Option>
-              <Option key="false">否</Option>
+              <Option value="">不限</Option>
+              <Option value="true">是</Option>
+              <Option value="false">否</Option>
             </Select>
             <Select key="3" placeholder="项目集合" onChange={projectChange} >
-              <Option key="">不限</Option>
+              <Option value="">不限</Option>
               {projectSets.map((proj) => (
                 <Option key={proj.id}>{proj.title}</Option>
               ))}
@@ -158,13 +159,15 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
         pagination={{
           pageSize: 5,
           showSizeChanger: true,
+          pageSizeOptions:[5,10,20],
         }}
         toolBarRender={() => [
-          <Button key="cancel" onClick={onCancel}>
+          <Button key="cancel" size="compact" onClick={onCancel}>
             取消
           </Button>,
           <Button
-            type="primary"
+            look="primary"
+            size="compact"
             icon={<PlusOutlined />}
             onClick={onEvaluate}
             key="add_evaluate"
@@ -172,7 +175,8 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
             新增评估
           </Button>,
           <Button
-            type="primary"
+            look="primary"
+            size="compact"
             icon={<PlusOutlined />}
             onClick={onTrain}
             key="add_train"
@@ -185,7 +189,7 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
             title: "训练前模型",
             dataIndex: "model_title_version",
             render: (v,record) => {
-              return (record.model_title_version? <Button type="link">{v}</Button> : '/');
+              return (record.model_title_version? <a onClick={() => { onAccuracy(record.new_model);}}>{v}</a> : '/');
             },
           },
           {
@@ -236,8 +240,8 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
           {
             title: "新模型",
             dataIndex: "new_model_title_version",
-            render: (v) => {
-              return <Button type="link">{v}</Button>;
+            render: (v,record) => {
+              return <a onClick={() => { onAccuracy(record.new_model);}}>{v}</a>;
             },
           },
           {
@@ -308,7 +312,6 @@ export default ({ onCancel, onEvaluate, onTrain }) => {
                   </Popconfirm>
                 ):'删除'
               );
-
             },
           },
         ]}
---
+++ label_studio/frontend/src/pages/DataManager/DataOperate/HumanInTheLoop/ModelAccuracy.js
@@ -0,0 +1,120 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Table } from "antd";
+import { Modal } from "@/components/Modal/Modal";
+import { Space } from "@/components/Space/Space";
+import { Button } from "@/components/Button/Button";
+import { ApiContext } from '@/providers/ApiProvider';
+import { format } from 'date-fns';
+import { Userpic } from '@/components';
+
+export default ({ onCancel ,modelId }) => {
+  const api = useContext(ApiContext);
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const formatNumber = (value) => {
+    if (!value)
+      value = 0;
+    try {
+      value = parseFloat(value) * 100;
+    } catch (e) {
+      value = 0;
+    }
+    return new Intl.NumberFormat('en', {
+      style: 'unit',
+      unit: 'percent',
+    }).format(value);
+  };
+
+  const getListData = async (pk) => {
+    if (!pk) {
+      return [];
+    }
+    return await api.callApi("accuracy", {
+      params: {
+        pk,
+      },
+    });
+  };
+
+  const columns = [
+    {
+      title: '模型',
+      dataIndex: 'new_model_title_version',
+      key: 'new_model_title_version',
+    },
+    {
+      title: '项目',
+      dataIndex: 'project_title',
+      key: 'project_title',
+    },
+    {
+      title: "准确率",
+      dataIndex: "accuracy_rate",
+      render: (_,record) => {
+        return (
+          <div>
+            {formatNumber(record.accuracy_rate)}
+          </div>
+        );
+      },
+    },
+    {
+      title: "时间",
+      dataIndex: "updated_at",
+      render: (_, record) => {
+        return (
+          <div name="created-date">
+            {format(new Date(record.created_at), "yyyy-MM-dd HH:mm")}
+          </div>
+        );
+      },
+    },
+    {
+      title: "操作人",
+      dataIndex: 'created_by',
+      render: (_, record) => {
+        return (
+          <div name="created-by">
+            <Userpic src="#" user={record.created_by} showUsername />
+          </div>
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    setLoading(true);
+    getListData(modelId).then(data => {
+      setLoading(false);
+      setDataSource(data);
+    });
+  }, [modelId]);
+
+
+  return (
+    <div className="evaluate">
+      <Modal.Header>
+        <span>
+          模型准确率
+        </span>
+      </Modal.Header>
+      <Table
+        style={{ paddingLeft:24,paddingRight:24,marginBottom:-1 }}
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 5,
+          showSizeChanger: true,
+          pageSizeOptions:[5,10,20],
+        }}
+        loading={loading} />
+      <Modal.Footer>
+        <Space align="end">
+          <Button size="compact" onClick={onCancel}>返回</Button>
+        </Space>
+      </Modal.Footer>
+    </div>
+  );
+};
