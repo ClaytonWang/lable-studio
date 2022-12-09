3b8ff7c99b88f6c64478c9b91fdf189dfb684c81
junshi <junshi.wang@digitalbrain.cn>
2022-12-09 15:19:12 +0800

    bug fix:remove lable config

--- label_studio/frontend/src/pages/DataManager/DataOperate/Prediction/IntentResponse.js
+++ label_studio/frontend/src/pages/DataManager/DataOperate/Prediction/IntentResponse.js
@@ -1,56 +1,24 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Col, Form, Row, Tag } from 'antd';
+import React, { useCallback, useEffect } from 'react';
+import { Form, Tag } from 'antd';
 import { Modal } from "@/components/Modal/Modal";
 import { Space } from "@/components/Space/Space";
 import { Button } from "@/components/Button/Button";
 import { Select } from '@/components/Form';
-import { useProject } from '@/providers/ProjectProvider';
-import { ConfigureControl } from '@/pages/CreateProject/Config/Config';
-import { Template } from '@/pages/CreateProject/Config/Template';
 import { useAPI } from '@/providers/ApiProvider';
-
-const { confirm } = Modal;
+import { useProject } from '@/providers/ProjectProvider';
 
 const formItemLayout = {
   labelCol: { span: 5 },
   wrapperCol: { span: 19 },
 };
 
-const IntentResponse = ({ close, execLabel }) => {
+const IntentResponse = ({ close, execLabel, loading }) => {
   const { project } = useProject();
-  const [template, setCurrentTemplate] = useState(null);
-  const [config, _setConfig] = React.useState("");
   const [execModel, setExecModel] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
   const [execModelList, setExecModelList] = React.useState([]);
-  const [modelLabels, setModelLabels] = React.useState([]);
   const api = useAPI();
 
-  const setConfig = useCallback(config => {
-    _setConfig(config);
-  }, [_setConfig]);
-
-  const setTemplate = useCallback(config => {
-    const tpl = new Template({ config });
-
-    tpl.onConfigUpdate = setConfig;
-    setConfig(config);
-    setCurrentTemplate(tpl);
-  }, [setConfig, setCurrentTemplate]);
-
-  const getModelLabels = useCallback(async (model_id) => {
-    if (model_id) {
-      const data = await api.callApi("modelLabel", {
-        params: {
-          model_id,
-        },
-      });
-
-      setModelLabels(data);
-    }
-    setExecModel(model_id);
-
-  }, []);
+  const projectLabels = project?.parsed_label_config?.intent?.labels ?? [];
 
   const getModelList = useCallback(async () => {
     const data = await api.callApi("modelList", {
@@ -64,68 +32,17 @@ const IntentResponse = ({ close, execLabel }) => {
   }, []);
 
   useEffect(() => {
-    if (project.label_config) {
-      setTemplate(project.label_config);
-    }
     getModelList();
   }, []);
 
-  const saveConfig = useCallback(async () => {
-    setLoading(true);
-    const res = await api.callApi("updateProjectRaw", {
-      params: {
-        pk: project.id,
-      },
-      body: {
-        label_config: config,
-      },
-    });
-
-    if (res.ok) {
-      execLabel({ model_id: execModel });
-      return true;
-    }
-    const error = await res.json();
-
-    setLoading(false);
-    return error;
-  }, [project, config, execModel]);
-
   const exec = () => {
-    let changedLabels = [];
-    const labels = project?.parsed_label_config?.intent?.labels ?? [];
-
-    template.controls.map(ctl => {
-      changedLabels = Array.from(ctl.children).map(c => {
-        return c.getAttribute("value");
-      });
-    });
 
     if (!execModel) {
       return;
     }
 
-    //判断新旧标签是否相同
-    const isSame = labels.length === changedLabels.length
-      && (labels.filter(t => !changedLabels.includes(t))).length === 0
-      && (changedLabels.filter(t => !labels.includes(t))).length === 0;
-
-    if (!isSame) {
-      confirm({
-        title: "警告",
-        body: "模型标签和已设置标签不一致是否继续标注?",
-        buttonLook: "destructive",
-        onOk: () => {
-          //go exec
-          saveConfig();
-        },
-        okText: "继续标注",
-        cancelText: "返回修改",
-      });
-    } else {
-      //go exec
-      saveConfig();
-    }
+    //go exec
+    execLabel({ model_id: execModel });
   };
 
   return (
@@ -150,39 +67,24 @@ const IntentResponse = ({ close, execLabel }) => {
                 return { label: v.title + v.version, value: v.id };
               })}
               onChange={(e) => {
-                getModelLabels(e.target.value);
+                setExecModel(e.target.value);
               }} />
           </div>
         </Form.Item>
         <Form.Item
-          label="所选模型标签"
+          label="项目标签"
         >
           <Space size="small">
             {
-              modelLabels && modelLabels.map(tag => {
+              projectLabels && projectLabels.map(tag => {
                 return (
                   <Tag key={tag}>{tag}</Tag>
                 );
               })
             }
           </Space>
-          {
-            modelLabels.length > 0 && <div style={{ color: 'red', marginTop: 10 }}>注:所选模型标签和已设置标签</div>
-          }
         </Form.Item>
       </Form>
-      {template && template.controls.map(control => {
-        return (
-          <Row key={control.getAttribute("name")} className={'configure-row'}>
-            <Col span={5} className={'ant-form-item-label'}>
-              <label>已设置标签</label>
-            </Col>
-            <Col span={19} >
-              <ConfigureControl control={control} template={template} />
-            </Col>
-          </Row>
-        );
-      })}
       <Modal.Footer>
         <Space align="end">
           <Button
