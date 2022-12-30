f54236342d4bec77c9c01aa024d4a6845cfe32e0
yjd <jindu.yin@digitalbrain.cn>
2022-12-30 10:56:35 +0800

    关掉redis的端口映射

--- label_studio/core/settings/qa_settings.py
+++ label_studio/core/settings/qa_settings.py
@@ -19,8 +19,8 @@ DATABASES = {
     }
 }
 
-# REDIS_HOST = '192.168.0.216'
-REDIS_HOST = 'redis-prod'
+REDIS_HOST = '192.168.0.216'
+# REDIS_HOST = 'redis-prod'
 RQ_QUEUES = {
     'default': {
         'HOST': REDIS_HOST,
