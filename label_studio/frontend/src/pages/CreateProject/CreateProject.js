910b984f19c9554c53172e244b28b345a295eb53
junshi <junshi.wang@digitalbrain.cn>
2023-01-10 16:48:18 +0800

    bug fix

--- label_studio/frontend/src/pages/CreateProject/CreateProject.js
+++ label_studio/frontend/src/pages/CreateProject/CreateProject.js
@@ -234,7 +234,7 @@ export const CreateProject = ({ onClose }) => {
           <ToggleItems items={steps} active={step} onSelect={onSelect} />
 
           <Space>
-            <Button look="danger" size="compact" onClick={onDelete} waiting={waiting}>{t('Cancel')}</Button>
+            <Button size="compact" onClick={onDelete} waiting={waiting}>{t('Cancel')}</Button>
             <Button look="primary" size="compact" onClick={onCreate} waiting={waiting || uploading} disabled={!project || uploadDisabled || error || showDot || waiting}>{t("Save")}</Button>
           </Space>
         </Modal.Header>
